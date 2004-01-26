8082081057aa7ac2840263c57a3db10c856ee9d6
Steven Schubiger (stsc) <schubiger@cpan.org>
2004-01-26 22:48:26 +0000

    Import of SCHUBIGER/Tie-DiskUsage-0.02 from CPAN.
    gitpan-cpan-distribution: Tie-DiskUsage
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SCHUBIGER/Tie-DiskUsage-0.02.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     released

--- lib/Tie/DiskUsage.pm
+++ lib/Tie/DiskUsage.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 our @ISA = qw(Tie::StdHash);
 use Carp 'croak';
@@ -30,7 +30,7 @@ sub _parse_usage {
         require File::Which;
 	$DU_BIN = File::Which::which('du');
     }
-    open PIPE, "$DU_BIN @_ $path |" or exit 1;       
+    open PIPE, "$DU_BIN @_ $path & |" or exit 1;       
     my %usage;
     for (<PIPE>) {
         my($size, $item) = split;
@@ -59,14 +59,14 @@ Tie::DiskUsage - tie disk-usage to an hash.
 
 =head1 DESCRIPTION
 
-C<Tie::DiskUsage> ties the disk-usage, which is gathered
+Tie::DiskUsage ties the disk-usage, which is gathered
 from the output of C<du>, to an hash. The mandatory parameter 
 consists of the path to perform the du-command on e.g. F</etc>, 
 optional arguments to C<du> may be passed subsequently.
 
 By default the location of the du-command is to be
 assumed in F</usr/bin/du>; if C<du> cannot be found there, 
-C<File::Which> will attempt to gather its proper location.
+C<File::Which> will attempt to gather its former location.
 
 =head1 SEE ALSO
 
