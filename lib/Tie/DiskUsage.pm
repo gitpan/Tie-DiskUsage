d0a879c67eaf289049d2e3e345ad06b6ede1108b
Steven Schubiger (stsc) <schubiger@cpan.org>
2004-01-26 18:17:16 +0000

    Import of SCHUBIGER/Tie-DiskUsage-0.01 from CPAN.
    gitpan-cpan-distribution: Tie-DiskUsage
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SCHUBIGER/Tie-DiskUsage-0.01.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     released

---
+++ lib/Tie/DiskUsage.pm
@@ -0,0 +1,75 @@
+package Tie::DiskUsage;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+our @ISA = qw(Tie::StdHash);
+use Carp 'croak';
+use Tie::Hash;
+
+our $DU_BIN = '/usr/bin/du';
+
+sub TIEHASH {
+    my $class = shift;
+    return bless \%{&_parse_usage}, $class;
+}
+
+sub STORE {
+    croak __PACKAGE__," doesn't support storing";
+}
+
+sub DESTROY {}
+sub UNTIE   {}
+
+sub _parse_usage {
+    my $path = shift;
+    if ((!-e $DU_BIN || !-f $DU_BIN) && $] >= 5.008) { 
+        require File::Which;
+	$DU_BIN = File::Which::which('du');
+    }
+    open PIPE, "$DU_BIN @_ $path |" or exit 1;       
+    my %usage;
+    for (<PIPE>) {
+        my($size, $item) = split;
+	chomp($item);
+	$usage{$item} = $size;
+    }
+    close PIPE 
+      or croak "Couldn't close pipe to $DU_BIN: $!";
+    return \%usage;
+} 
+
+1;
+__END__
+
+=head1 NAME
+
+Tie::DiskUsage - tie disk-usage to an hash.
+
+=head1 SYNOPSIS
+
+ require Tie::DiskUsage;
+
+ tie %usage, 'Tie::DiskUsage', '/etc', '-h';
+ 
+ print $usage{'/etc'};
+
+=head1 DESCRIPTION
+
+C<Tie::DiskUsage> ties the disk-usage, which is gathered
+from the output of C<du>, to an hash. The mandatory parameter 
+consists of the path to perform the du-command on e.g. F</etc>, 
+optional arguments to C<du> may be passed subsequently.
+
+By default the location of the du-command is to be
+assumed in F</usr/bin/du>; if C<du> cannot be found there, 
+C<File::Which> will attempt to gather its proper location.
+
+=head1 SEE ALSO
+
+L<perlfunc/tie>, du(1).
+
+=cut
---
+++ t/Tie-DiskUsage.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 2;
+
+BEGIN {
+    my $PACKAGE = 'Tie::DiskUsage';
+    use_ok($PACKAGE);
+    require_ok($PACKAGE);
+}
