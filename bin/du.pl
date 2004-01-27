5cdfed9df82c4a3d805d26e58b68c12af93cb400
Steven Schubiger (stsc) <schubiger@cpan.org>
2004-01-27 08:14:32 +0000

    Import of SCHUBIGER/Tie-DiskUsage-0.03 from CPAN.
    gitpan-cpan-distribution: Tie-DiskUsage
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SCHUBIGER/Tie-DiskUsage-0.03.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     released

---
+++ bin/du.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Tie::DiskUsage;
+
+my $path = '/var';
+my $log  = "$path/log";
+
+tie my %usage, 'Tie::DiskUsage', $path, '-h';
+print "$log: $usage{$log}\n";
--- lib/Tie/DiskUsage.pm
+++ lib/Tie/DiskUsage.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 our @ISA = qw(Tie::StdHash);
 use Carp 'croak';
@@ -25,7 +25,7 @@ sub DESTROY {}
 sub UNTIE   {}
 
 sub _parse_usage {
-    my $path = shift;
+    my $path = shift || '.';
     if ((!-e $DU_BIN || !-f $DU_BIN) && $] >= 5.008) { 
         require File::Which;
 	$DU_BIN = File::Which::which('du');
@@ -53,20 +53,21 @@ Tie::DiskUsage - tie disk-usage to an hash.
 
  require Tie::DiskUsage;
 
- tie %usage, 'Tie::DiskUsage', '/etc', '-h';
- 
- print $usage{'/etc'};
+ tie %usage, 'Tie::DiskUsage', '/var', '-h';
+
+ print $usage{'/var/log'};
 
 =head1 DESCRIPTION
 
 Tie::DiskUsage ties the disk-usage, which is gathered
-from the output of C<du>, to an hash. The mandatory parameter 
-consists of the path to perform the du-command on e.g. F</etc>, 
-optional arguments to C<du> may be passed subsequently.
+from the output of C<du>, to an hash. If the path to perform 
+the du-command on e.g. F</etc> is being omitted, the current working 
+directory will be assumed; optional arguments to C<du> may be 
+passed subsequently.
 
 By default the location of the du-command is to be
-assumed in F</usr/bin/du>; if C<du> cannot be found there, 
-C<File::Which> will attempt to gather its former location.
+assumed in F</usr/bin/du>; if C<du> cannot be found to exist
+there, C<File::Which> will attempt to gather its former location.
 
 =head1 SEE ALSO
 
